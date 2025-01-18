b54e4760116b5e22baf4f6431d8ee9d0e2ad677a
guru <gurusaghi324@gmail.com>
2025-01-18 22:05:17 +0530

    cmmit

--- 12. Swap first last last digit of number/02_using_mathematical_op.cpp
+++ 12. Swap first last last digit of number/02_using_mathematical_op.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 int main() {
     int number = 12345;
     int temp = number, numDigits = 0;
@@ -21,8 +23,8 @@ int main() {
     // Reconstruct the number
     int swappedNumber = lastDigit * static_cast<int>(pow(10, numDigits - 1)) + middlePart * 10 + firstDigit;
 
-    std::cout << "Original number: " << number << std::endl;
-    std::cout << "Swapped number: " << swappedNumber << std::endl;
+    cout << "Original number: " << number << endl;
+    cout << "Swapped number: " << swappedNumber << endl;
 
     return 0;
 }
