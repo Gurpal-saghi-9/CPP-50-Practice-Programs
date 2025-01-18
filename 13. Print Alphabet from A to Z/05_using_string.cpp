7bb9ddce61869f816c327c10abcee76843f5c0f1
guru <gurusaghi324@gmail.com>
2025-01-18 22:16:21 +0530

    commit

---
+++ 13. Print Alphabet from A to Z/05_using_string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string uppercase = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+    string lowercase = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+
+    cout << "Uppercase Alphabets: " << uppercase << endl;
+    cout << "Lowercase Alphabets: " << lowercase << endl;
+
+    return 0;
+}
