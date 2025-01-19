4a617606fc44d0b32974a4a7a9928295a6d1eb57
guru <gurusaghi324@gmail.com>
2025-01-19 19:36:25 +0530

    commit

---
+++ 15. Find string length/04_using_strlen()_from_cstring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    const char* str = "Hello, World!";
+
+    cout << "Length using strlen(): " << strlen(str) << endl;
+    
+    return 0;
+}
