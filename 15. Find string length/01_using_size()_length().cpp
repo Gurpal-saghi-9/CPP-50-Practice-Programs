3555bda825e28b1c34d58f7b639246620339943c
guru <gurusaghi324@gmail.com>
2025-01-19 19:27:33 +0530

    commit

---
+++ 15. Find string length/01_using_size()_length().cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str = "Hello, World!";
+    
+    cout << "Length using size(): " << str.size() << endl;
+    cout << "Length using length(): " << str.length() << endl;
+
+    return 0;
+}
