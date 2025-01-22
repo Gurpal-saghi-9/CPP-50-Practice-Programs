b6b26ea9c1d732d227e3ca01793ae253484f83a5
guru <gurusaghi324@gmail.com>
2025-01-22 20:38:11 +0530

    commit

---
+++ 39. Total number of negetive element in array/02_using_dynamic_array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int* arr = new int[n]; // Dynamically allocate memory
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (arr[i] < 0) {
+            count++;
+        }
+    }
+
+    cout << "Total number of negative elements: " << count << endl;
+
+    delete[] arr; // Free allocated memory
+
+    return 0;
+}
